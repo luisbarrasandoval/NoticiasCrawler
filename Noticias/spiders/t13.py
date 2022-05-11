3b57b3f4c25af39f8f4cbfcf0f4be55213a1aa84
Luis Barra <luisbarra454@gmail.com>
2022-05-11 15:44:43 -0400

    t13

--- Noticias/spiders/t13.py
+++ Noticias/spiders/t13.py
@@ -4,6 +4,7 @@ from Noticias.items import NoticiasItem
 
 NOTICIAS = '.card-normal'
 
+
 class T13(scrapy.Spider):
     name = 't13'
     allowed_domains = ['www.t13.cl']
@@ -12,11 +13,20 @@ class T13(scrapy.Spider):
         'https://www.t13.cl/mundo'
     ]
 
-
     def parse(self, response):
         item = NoticiasItem()
         for noticia in response.css(NOTICIAS):
-            item["url"] =  response.urljoin(noticia.css("a")[0].attrib['href'])
+            item["url"] = response.urljoin(noticia.css("a")[0].attrib['href'])
             item["title"] = noticia.css("h2::text").get()
             item["img"] = noticia.css("img")[0].attrib['src']
-            yield item
+            item['description'] = noticia.css("img")[0].attrib['alt']
+            yield scrapy.Request(
+                item["url"],
+                callback=self.parse_page,
+                meta={'item': item}
+            )
+
+    def parse_page(self, response):
+        item = response.meta['item']
+        # falta
+        yield item
